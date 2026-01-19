00f08c2e70b971fb195116aa285e4adc4e0d0ea0
6arleyhuman <kosov.kiril2@gmail.com>
2026-01-19 14:40:01 +0200

    add sample code for Sonar issues
    Refactor Program.cs to include a method for message retrieval and improve structure.

--- Program.cs
+++ Program.cs
@@ -1,2 +1,48 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        // Поведінка лишається: друкуємо "Hello, World!"
+        Console.WriteLine(GetMessage(args));
+    }
+
+    // Тут навмисно "smells", щоб Sonar було що знайти
+    private static string GetMessage(string[] args)
+    {
+        string msg = "Hello, World!";
+        string tmp = msg; // дубль/зайве
+        int magic = 42;   // магічне число
+        if (magic == 42) { } // порожній блок
+
+        try
+        {
+            // нічо не робимо
+        }
+        catch (Exception)
+        {
+            // порожній catch
+        }
+
+        if (args != null && args.Length > 0 && args[0] != null && args[0].Length > 0)
+        {
+            msg = args[0];
+        }
+
+        // зайва умова
+        if (msg == msg)
+        {
+            msg = msg;
+        }
+
+        // мертвий код
+        if (false)
+        {
+            msg = "never";
+        }
+
+        return tmp;
+    }
+}
+
