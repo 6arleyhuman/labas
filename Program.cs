3f7c67e4f904b031876b434c514e01963f38ba2d
6arleyhuman <kosov.kiril2@gmail.com>
2026-01-19 14:55:28 +0200

    add code smells for lab2
    Refactor Program.cs to include a GetMessage method that returns a message based on input arguments.

--- Program.cs
+++ Program.cs
@@ -1,4 +1,45 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-// lab2 test
+using System;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine(GetMessage(args));
+    }
+
+    private static string GetMessage(string[] args)
+    {
+        string msg = "Hello, World!";
+        string tmp = msg;
+
+        int magic = 42;
+        if (magic == 42) { }
+
+        try
+        {
+        }
+        catch (Exception)
+        {
+        }
+
+        if (args != null && args.Length > 0 && args[0] != null && args[0].Length > 0)
+        {
+            msg = args[0];
+        }
+
+        if (msg == msg)
+        {
+            msg = msg;
+        }
+
+        if (false)
+        {
+            msg = "never";
+        }
+
+        return tmp;
+    }
+}
+
+
 
